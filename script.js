58f053f1004e3286564fab89bc6500739f3c52fd
maciekgaluszka <maciej.galuszka01@gmail.com>
2025-06-20 09:49:50 +0200

    project files added

---
+++ script.js
@@ -0,0 +1,55 @@
+const menuButton = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector("header");
+
+menuButton.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
+
+console.log("toggled");
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (
+      link.classList.contains("nav-link") ||
+      link.classList.contains("nav-cta")
+    )
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
